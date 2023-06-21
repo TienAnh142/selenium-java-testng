a88344f3457ede7ee01ffc32c5534a953b06dd6b
Tran Tien Anh <trantienanh.01042000@gmail.com>
2023-06-21 23:13:53 +0700

    Intial commit

--- selenium/webdriver/Topic_01_Xpath_Css.java
+++ selenium/webdriver/Topic_01_Xpath_Css.java
@@ -1,9 +1,11 @@
 package webdriver;
 
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class Topic_01_Xpath_Css {
-
+	WebDriver driver;
+	
 	@Test
 	public void TC_001() {
 		
